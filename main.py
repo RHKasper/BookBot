238850a85d80d56a151c51d7b1884a44e3f63a19
RHKasper <RobertHKasper@gmail.com>
2023-08-29 23:38:30 -0400

    Modifies main.py to print the word count of the given book text

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-
 def main():
     path_to_file = "books/frankenstein.txt"
     text = get_book_text(path_to_file)
-    print(text)
+    print("Hello world")
+    print("Word Count: " + str(get_word_count(text)))
 
 def get_book_text(path):
-    with open(path_to_file) as file:
+    with open(path) as file:
         file_contents = file.read()
         return file_contents
+
+def get_word_count(string):
+    return len(string.split())
+
+main()
